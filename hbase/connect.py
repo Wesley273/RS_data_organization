e5b830e96c7042ae3ab756f80ecf92a98a524cc9
wesley <menglz2128@outlook.com>
2022-03-03 16:10:17 +0800

    Update

--- hbase/connect.py
+++ hbase/connect.py
@@ -1,6 +1,16 @@
+
 import happybase
-connection = happybase.Connection('localhost', port=9090, autoconnect=False)
-connection.open()
-# see all tables in hbase
-print(connection.tables())
-connection.close()
+
+# remenber to run'hbase thrift -p 9090 start' in cmd
+
+
+def open_connection():
+    connection = happybase.Connection('localhost', port=9090, autoconnect=False)  # 连接到hbase
+    connection.open()
+    return connection
+
+
+if __name__ == "__main__":
+    connection = open_connection()
+    print(connection.tables())  # 查看hbase现有的所有表名
+    table = connection.table('MODIS')
