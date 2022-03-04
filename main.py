a6e6f8cc828538b507d00f770630cdcadb3f7b36
wesley <menglz2128@outlook.com>
2022-03-04 14:55:44 +0800

    Update

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import rasterio
 
 
-def test():
+def basic_test():
     with rasterio.open(r'data\cloud_free\NDSI_2021_02_01.tif') as ds:
         print('该栅格数据的基本数据集信息(这些信息都是以数据集属性的形式表示的):')
         print(f'数据格式:{ds.driver}')
         print(f'波段数目:{ds.count}')
         print(f'影像宽度:{ds.width}')
         print(f'影像高度:{ds.height}')
-        print(f'地理范围:{ds.bounds}')
+        print(f'地理坐标范围:{ds.bounds}')
         print(f'反射变换参数(六参数模型):\n {ds.transform}')
         print(f'投影定义:{ds.crs}')
         # 获取第一个波段数据，跟GDAL一样索引从1开始
@@ -17,16 +17,7 @@ def test():
         print(f'第一波段的最大值:{band1.max()}')
         print(f'第一波段的最小值:{band1.min()}')
         print(f'第一波段的平均值:{band1.mean()}')
-        # 根据地理坐标得到行列号,注意矩阵的第一维为图像的y轴
-        x, y = (ds.bounds.left + 4000, ds.bounds.top - 4000)  # 距离左上角东300米，南300米的投影坐标
-        row, col = ds.index(x, y)  # 对应的行列号
-        print(f'(投影坐标{x}, {y})对应的行列号是({row}, {col})')
-        # 根据行列号得到地理坐标
-        x, y = ds.xy(row, col)  # 中心点的坐标
-        print(f'行列号({row}, {col})对应的中心投影坐标是({x}, {y})')
-        # 那么如何得到对应点左上角的信息
-        x, y = (row, col)*ds.transform
-        print(f'行列号({row}, {col})对应的左上角投影坐标是({x}, {y})')
+        # 注意矩阵的第一维为图像的y轴
         # 观察点之间的坐标关系
         row = 7
         col = 7
@@ -34,8 +25,8 @@ def test():
         print(f'行列号({row}, {col})对应的左上角投影坐标是({x}, {y})')
         x, y = ds.xy(row, col)  # 中心点的坐标
         print(f'行列号({row}, {col})对应的中心投影坐标是({x}, {y})')
-        row = row
-        col += 1
+        row += 1
+        col = col
         x, y = (row, col)*ds.transform
         print(f'行列号({row}, {col})对应的左上角投影坐标是({x}, {y})')
         x, y = ds.xy(row, col)  # 中心点的坐标
@@ -49,4 +40,4 @@ def test():
 
 
 if __name__ == "__main__":
-    test()
+    basic_test()
