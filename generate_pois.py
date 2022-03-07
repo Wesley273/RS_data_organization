86aa5451bfe04811729e95b3dfadb90c1d06ee52
wesley <menglz2128@outlook.com>
2022-03-07 15:38:25 +0800

    Update generate_pois.py

--- generate_pois.py
+++ generate_pois.py
@@ -2,6 +2,7 @@ import random
 import rasterio
 from indexing.geo_points import POI
 from faker import Faker
+import csv
 
 
 def export2csv(points):
@@ -20,17 +21,22 @@ def gen_comment():
 
 def gen_pois(img_array):
     points = []
+    f = open(r'data\poi\test.csv', 'w', encoding='utf-8')
+    csv_writer = csv.writer(f)
+    csv_writer.writerow(["code", "date", "name", "lon", "lat", "cover_rate", "comment"])
     for x in range(len(img_array)):
         for y in range(len(img_array[x])):
             if(random.random()*100 < 0.01):
-                points.append(POI(20220118, x, y, gen_name(), img_array[x][y], gen_comment()))
+                poi = POI(20220118, x, y, gen_name(), img_array[x][y], gen_comment())
+                points.append(poi)
+                csv_writer.writerow([poi.code, poi.date, poi.name, poi.lon, poi.lat, poi.cover_rate, poi.comment])
                 print(img_array[x][y])
+    f.close()
     return(points)
 
 
 if __name__ == "__main__":
     ds = rasterio.open(r'data\img\NDSI_2022_01_18.tif')
-    
     img_array = ds.read(1)
     points = gen_pois(img_array)
     print(points[1])
