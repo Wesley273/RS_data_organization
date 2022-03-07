d52a10e1efd2e4347af4d4e23d5c9846490e7f9b
wesley <menglz2128@outlook.com>
2022-03-07 16:53:33 +0800

    Update

--- generate_pois.py
+++ generate_pois.py
@@ -15,7 +15,7 @@ def gen_name():
 
 def gen_comment():
     fake = Faker("zh_CN")
-    return fake.name()+fake.address()+fake.email()
+    return fake.name()+"负责,地址与联系方式: "+fake.address()+"; "+fake.email()+"; "+fake.phone_number()
 
 
 def gen_pois(csv_writer, img_array, date: int):
@@ -26,21 +26,22 @@ def gen_pois(csv_writer, img_array, date: int):
                 poi = POI(date, row, col, gen_name(), img_array[row][col], gen_comment())
                 points.append(poi)
                 csv_writer.writerow([poi.code, poi.date, poi.name, poi.lon, poi.lat, row, col, poi.cover_rate, poi.comment])
-                print(img_array[row][col])
+                #print("%2d " % (img_array[row][col]), end='')
     return(points)
 
 
 if __name__ == "__main__":
-    begin = datetime.date(2021, 2, 1)
-    end = datetime.date(2021, 2, 2)
+    begin = datetime.date(2021, 2, 3)
+    end = datetime.date(2021, 2, 4)
     file = open(r'data\poi\test.csv', 'a+', encoding='utf-8-sig', newline='')
     csv_writer = csv.writer(file)
     csv_writer.writerow(["code", "date", "name", "lon", "lat", "row", "col", "cover_rate", "comment"])
     # 遍历对每天的遥感图像采样
     for i in range((end - begin).days):
         day = begin + datetime.timedelta(days=i)
+        print(f"对 {day} 日图像采样····· ")
         ds = rasterio.open(f'data\\img\\NDSI_{str(day).replace("-", "_", 2)}.tif')
         img_array = ds.read(1)
         points = gen_pois(csv_writer, img_array, int(str(day).replace("-", "", 2)))
-        print(f"{day}所含点: ", len(points))
+        print(f"{day} 所含点数: ", len(points))
     file.close()
